01e4f571c7d51d46ce22f98ac46744f46d524454
sakura1025 <fulein1025@163.com>
2014-12-25 19:39:14 +0800

    完成了

--- WebGather/HttpUtil.cs
+++ WebGather/HttpUtil.cs
@@ -47,6 +47,49 @@ namespace WebGather
             }
             return request.GetResponse() as HttpWebResponse;
         }
+
+        /// <summary>  
+        /// 创建GET方式的HTTP请求  
+        /// </summary>  
+        /// <param name="url">请求的URL</param>  
+        /// <param name="timeout">请求的超时时间</param>  
+        /// <param name="userAgent">请求的客户端浏览器信息，可以为空</param>  
+        /// <param name="cookies">随同HTTP请求发送的Cookie信息，如果不需要身份验证可以为空</param>  
+        /// <returns></returns>  
+        public static string GetHttpResponse(string url, int? timeout, string userAgent, CookieCollection cookies,Encoding responseEncoding)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "GET";
+            request.UserAgent = DefaultUserAgent;
+            if (!string.IsNullOrEmpty(userAgent))
+            {
+                request.UserAgent = userAgent;
+            }
+            if (timeout.HasValue)
+            {
+                request.Timeout = timeout.Value;
+            }
+            if (cookies != null)
+            {
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+            }
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            if (response == null)
+            {
+                return string.Empty;
+            }
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, responseEncoding);
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
         /// <summary>  
         /// 创建POST方式的HTTP请求  
         /// </summary>  
@@ -124,6 +167,96 @@ namespace WebGather
                 }
             }
             return request.GetResponse() as HttpWebResponse;
+        }
+
+        /// <summary>  
+        /// 创建POST方式的HTTP请求  
+        /// </summary>  
+        /// <param name="url">请求的URL</param>  
+        /// <param name="parameters">随同请求POST的参数名称及参数值字典</param>  
+        /// <param name="timeout">请求的超时时间</param>  
+        /// <param name="userAgent">请求的客户端浏览器信息，可以为空</param>  
+        /// <param name="requestEncoding">发送HTTP请求时所用的编码</param>  
+        /// <param name="cookies">随同HTTP请求发送的Cookie信息，如果不需要身份验证可以为空</param>  
+        /// <returns></returns>  
+        public static string PostHttpResponse(string url, IDictionary<string, string> parameters, int? timeout, string userAgent, Encoding requestEncoding, CookieCollection cookies, Encoding responseEncoding)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+            if (requestEncoding == null)
+            {
+                throw new ArgumentNullException("requestEncoding");
+            }
+            HttpWebRequest request = null;
+            //如果是发送HTTPS请求  
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                request = WebRequest.Create(url) as HttpWebRequest;
+                request.ProtocolVersion = HttpVersion.Version10;
+            }
+            else
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+            }
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            if (!string.IsNullOrEmpty(userAgent))
+            {
+                request.UserAgent = userAgent;
+            }
+            else
+            {
+                request.UserAgent = DefaultUserAgent;
+            }
+
+            if (timeout.HasValue)
+            {
+                request.Timeout = timeout.Value;
+            }
+            if (cookies != null)
+            {
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+            }
+            //如果需要POST数据  
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                StringBuilder buffer = new StringBuilder();
+                int i = 0;
+                foreach (string key in parameters.Keys)
+                {
+                    if (i > 0)
+                    {
+                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                    }
+                    else
+                    {
+                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                    }
+                    i++;
+                }
+                byte[] data = requestEncoding.GetBytes(buffer.ToString());
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            if (response == null)
+            {
+                return string.Empty;
+            }
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, responseEncoding);
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
         }
 
         private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
--- WebGather/Program.cs
+++ WebGather/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using Newtonsoft;
 using System.Net;
 using Newtonsoft.Json;
@@ -12,6 +13,8 @@ using System.Data.Entity;
 using System.Collections;
 using System.Threading;
 using log4net;
+using System.Runtime.InteropServices;
+using System.Reflection;
 
 
 namespace WebGather
@@ -27,62 +30,23 @@ namespace WebGather
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //log4net.Config.XmlConfigurator.Configure();
-            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo(@"C:\Users\SunSa\documents\visual studio 2012\Projects\WebGather\WebGather\log4net.xml"));
             // Application.Run(new Form1());
-            //var response = HttpUtil.GetHttpResponse("http://www.aspjzy.com/13430.html", null, null, null);
 
-            //Stream myResponseStream = response.GetResponseStream();
-            //StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.Default);
-            //string retString = myStreamReader.ReadToEnd();
-            //myStreamReader.Close();
-            //myResponseStream.Close();
-            ////Console.WriteLine(retString);
-            //File.WriteAllText("c://test.txt", retString);
+            Gather gt = new Gather(log);
+            gt.Start();
 
-            using (var db = new GatherContext())
+
+            while (true)
             {
-                log.DebugFormat("asdfasdfasdf{0}", "孙沙");
-                //var name = Console.ReadLine();
-                try
-                {
-                    var model = new Question { Title = DateTime.Now.Ticks.ToString() };
-
-                    //model.QuesAbilityModelId = 1;
-                    model.QuesAbility = new QuesAbilityModel();
-                    model.QuesAbility.ID = 19;
-                    model.QuesAbilityModelId = 19;
-                    model.QuesAbility.Name = "asdfasdfa";
-                    model.QuesTypeModelId = 1;
-                    model.QuesDiffModelId = 1;
-                    db.Questions.Add(model);
-                    // db.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-
-                    Console.WriteLine(ex.ToString());
-                }
-                //Display all Blogs from the database 
-                var query = from b in db.Questions
-                            orderby b.ID
-                            select b;
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.ID + "\t" + item.Title);
-                    // Console.WriteLine(item.QuesAbility.ID);
-                }
-
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
+                Console.WriteLine("主线程休眠10秒");
+                Thread.Sleep(1000 * 100);
             }
 
-            //Test();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            }
 
-        }
-
+     
         static void Test()
         {
             string json = string.Empty;
@@ -91,6 +55,307 @@ namespace WebGather
             Console.Read();
         }
     }
+
+    public class Gather
+    {
+
+        public static int StopSecond = 1;
+        public static int ThreadRequestMaxNum = 1;
+        public static int ThreadResultMaxNum = 1;
+
+        Queue<WebPage> queWebPage = new Queue<WebPage>();
+
+        Queue<Dictionary<string, string>> queResult = new Queue<Dictionary<string, string>>();
+
+        Thread[] threadRequest = new Thread[ThreadRequestMaxNum];
+        Thread[] threadResult = new Thread[ThreadResultMaxNum];
+        private log4net.ILog log;
+
+        public Gather(log4net.ILog mlog)
+        {
+            log = mlog;
+            queWebPage.Enqueue(new WebPage { IsListPage = true, Url = "http://dyzsks.com/KuaiXun", PageEncoding = Encoding.UTF8, RegexContent = new Regex("<div class=\"article\">(?<desc>[\\s\\S]+?)</div>\\s*</div>\\s*</div>\\s*<div class=\"rightColumn clearfix\">"), RegexList = new Regex("<li><a href=\"(?<contenturl>[\\s\\S]+?)\" style=\"color:#\\d*\" target=\"_blank\" title=\"(?<title>[\\s\\S]+?)\">[\\s\\S]+?</a></li>"), RegexParam = new Regex("<title>(?<webname>[\\s\\S]+?)</title>"), ParamObj = null });
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < ThreadRequestMaxNum; i++)
+            {
+                threadRequest[i] = new Thread(new ThreadStart(ProcessWebPage));
+                threadRequest[i].Name = "ProcessWebPage" + i;
+                threadRequest[i].IsBackground = true;
+                threadRequest[i].Start();
+            }
+
+            for (int i = 0; i < ThreadResultMaxNum; i++)
+            {
+                threadResult[i] = new Thread(new ThreadStart(ProcessResult));
+                threadResult[i].Name = "ProcessResult" + i;
+                threadResult[i].IsBackground = true;
+                threadResult[i].Start();
+            }
+
+        }
+
+        public void Stop()
+        {
+            for (int i = 0; i < ThreadRequestMaxNum; i++)
+            {
+                if (threadRequest[i] != null && threadRequest[i].IsAlive == true)
+                {
+                    threadRequest[i].Abort();
+                    threadRequest[i] = null;
+                }
+            }
+
+            for (int i = 0; i < ThreadResultMaxNum; i++)
+            {
+                if (threadResult[i] != null && threadResult[i].IsAlive == true)
+                {
+                    threadResult[i].Abort();
+                    threadResult[i] = null;
+                }
+            }
+        }
+
+        private Dictionary<string, string> Merger(Dictionary<string, string> firstDic,Dictionary<string, string> secondDic)
+        {
+            if (firstDic == null)
+            {
+                return secondDic;
+            }
+            if (secondDic == null)
+            {
+                return null;
+            }
+            foreach (var kp in secondDic)
+            {
+                if (firstDic.ContainsKey(kp.Key))
+                {
+                    firstDic[kp.Key] = kp.Value;
+                }
+                else
+                {
+                    firstDic.Add(kp.Key,kp.Value);
+                }
+            }
+            return firstDic;
+
+        }
+
+        public void ProcessWebPage()
+        {
+            while (true)
+            {
+                WebPage wp = null;
+                if (queWebPage.Count > 0)
+                {
+                   wp = queWebPage.Dequeue();
+                }
+                if (null == wp)
+                {
+                    log.InfoFormat("weburl对象为空，页面队列数量{0}，暂停{1}秒", queWebPage.Count, StopSecond);
+                    Thread.Sleep(1000 * StopSecond);
+                    continue;
+                }
+                string HtmlContent = HttpUtil.GetHttpResponse(wp.Url, 5000, null, null,wp.PageEncoding);
+                if (string.IsNullOrEmpty(HtmlContent))
+                {
+                    log.InfoFormat("weburl的Url：{0}，获取内容为空字符串，忽略", wp.Url);
+                    continue;
+                }
+                if (!wp.IsListPage && null != wp.RegexContent)
+                {
+                    Dictionary<string, string> dic = new Dictionary<string, string>();
+
+                    Match mContent = wp.RegexContent.Match(HtmlContent);
+                    if (mContent != null && mContent.Groups.Count > 0)
+                    {
+                        foreach (string key in wp.RegexContent.GetGroupNames())
+                        {
+                            if (key == "0")
+                                        continue;
+                            dic.Add(key, mContent.Groups[key].Value);
+                        }
+                    }
+                    if (dic != null)
+                    {
+                        if (wp.ParamObj != null)
+                        {
+                            dic = Merger(dic, (Dictionary<string, string>)wp.ParamObj);
+                        }
+                        if (dic.ContainsKey("morecontenturl"))
+                        {
+                            WebPage nextwp = new WebPage();
+                            nextwp.Url = "http://dyzsks.com"+dic["morecontenturl"];
+                            nextwp.PageEncoding = wp.PageEncoding;
+                            nextwp.ParamObj = dic;
+                            nextwp.RegexList = null;
+                            nextwp.RegexParam = null;
+                            nextwp.RegexContent = wp.RegexContent;
+                            nextwp.IsListPage = false;
+                            queWebPage.Enqueue(nextwp);
+                            dic.Remove("morecontenturl");
+                        }
+                        else
+                        {
+                            queResult.Enqueue(dic);
+                        }
+                        if (queWebPage.Count > 1000)
+                        {
+                            log.InfoFormat("weburl对象为空，页面队列数量{0}，暂停{1}秒", queWebPage.Count, StopSecond * 10);
+                            System.Threading.Thread.Sleep(1000 * 10 * StopSecond);
+                        }
+                    }
+                }
+                else
+                {
+                    if (null != wp.RegexParam)
+                    {
+                        Match mParam = wp.RegexParam.Match(HtmlContent);
+                        if (mParam != null && mParam.Groups.Count > 0)
+                        {
+                            Dictionary<string, string> dic = new Dictionary<string, string>();
+                            foreach (string key in wp.RegexParam.GetGroupNames())
+                            {
+                                if (key == "0")
+                                    continue;
+                                dic.Add(key, mParam.Groups[key].Value);
+                            }
+                            if (wp.ParamObj != null)
+                            {
+                                dic = Merger(dic, (Dictionary<string, string>)wp.ParamObj);
+                            }
+                            wp.ParamObj = dic;
+                        }
+                    }
+                    if (null != wp.RegexList)
+                    {
+                        MatchCollection mcList = wp.RegexList.Matches(HtmlContent);
+                        foreach (Match m in mcList)
+                        {
+                            if (null != m && !string.IsNullOrEmpty(m.Groups["contenturl"].Value))
+                            {
+                                Dictionary<string, string> dic = new Dictionary<string, string>();
+                                foreach (string key in wp.RegexList.GetGroupNames())
+                                {
+                                    if (key == "0")
+                                        continue;
+                                    dic.Add(key, m.Groups[key].Value);
+                                }
+                                if (wp.ParamObj != null)
+                                {
+                                 // wp.ParamObj=  dic.Union((Dictionary<string, string>)wp.ParamObj);
+                                    dic = Merger(dic, (Dictionary<string, string>)wp.ParamObj);
+                                }
+                                WebPage nextwp = new WebPage();
+                                nextwp.Url = "http://dyzsks.com" + m.Groups["contenturl"].Value;
+                                nextwp.PageEncoding = wp.PageEncoding;
+                                nextwp.ParamObj = dic;
+                                nextwp.RegexList = null;
+                                nextwp.RegexParam = null;
+                                nextwp.RegexContent = wp.RegexContent;
+                                nextwp.IsListPage = false;
+                                queWebPage.Enqueue(nextwp);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+            
+
+        
+
+        public void ProcessResult()
+        {
+            while (true)
+            {
+                Dictionary<string, string> dicresult = new Dictionary<string, string>();
+                if (queResult.Count > 0)
+                {
+                    dicresult = queResult.Dequeue();
+                    File.AppendAllText("c:\\Testasdf.txt)",string.Format("附带参数:{0}\n标题:{1}\n网址:{3}\n详细内容:{2}\n------------------------------\n\n", dicresult["webname"], dicresult["title"], dicresult["desc"], dicresult["contenturl"]));
+                }
+                const int StopSencond = 3;
+                if (null == dicresult||dicresult.Keys.Count==0)
+                {
+                    log.InfoFormat("dicresult对象为空，结果队列数量{0}，暂停{1}秒", queResult.Count, StopSencond);
+                    Thread.Sleep(1000 * StopSencond);
+                    continue;
+                }
+            }
+        }
+    }
+
+
+    #region Match帮助类
+
+    public class MatchHelper
+    {
+        public static T PopulateEntityFromCollection<T>(T entity, Match m) where T : new()
+        {
+            //初始化 如果为null
+            if (entity == null)
+            {
+                entity = new T();
+            }
+            //得到类型
+            Type type = typeof(T);
+            //取得属性集合
+            PropertyInfo[] pi = type.GetProperties();
+            foreach (PropertyInfo item in pi)
+            {
+                string value = m.Groups[item.Name.ToLower()].Value;
+                //给属性赋值
+                if (m.Groups[item.Name.ToLower()].Value != null)
+                {
+                    
+                    item.SetValue(entity, string.IsNullOrEmpty(value) ? null : Convert.ChangeType(value, item.PropertyType), null);
+                }
+            }
+            return entity;
+        }
+    }
+
+    public interface IMatchToModel
+    {
+        dynamic GetModel(string input);
+    }
+
+
+    #endregion
+
+    #region 采集类
+
+    public class WebPage
+    {
+        public string Url { get; set; }
+        public Encoding PageEncoding { get; set; }
+        public Regex RegexList { get; set; }
+        public Regex RegexParam { get; set; }
+        public Regex RegexContent { get; set; }
+        public dynamic ParamObj { get; set; }
+        public bool IsListPage { get; set; }
+    }
+
+    public class WebRegex
+    {
+        public string Url { get; set; }
+        public Regex RegexStr { get; set; }
+    }
+
+    public enum RegexTypeEnum
+    {
+        Content = 1,
+        List,
+        Param
+    }
+
+    #endregion
+
+    #region 试卷类
+
 
     public class Question
     {
@@ -141,6 +406,13 @@ namespace WebGather
         public string Name { get; set; }
     }
 
+    #endregion
+
+    #region 数据访问
+
+    /// <summary>
+    /// 数据库访问
+    /// </summary>
     public class GatherContext : DbContext
     {
         public DbSet<Question> Questions { get; set; }
@@ -149,112 +421,5 @@ namespace WebGather
         public DbSet<QuesTypeModel> QuesType { get; set; }
     }
 
-    public class WebOperation
-    {
-
-    }
-
-    public class IWebOperation<T>
-    {
-
-    }
-
-
-    public class WebMain2
-    {
-        public Queue<WebUrl> QuePage { get; set; }
-
-        public void Run()
-        {
-
-        }
-
-
-
-        public void Func(WebUrl wp)
-        {
-            string url = "";
-            HttpWebResponse response = HttpUtil.GetHttpResponse(url, 5000, null, null);
-            if (response == null)
-            {
-                Console.WriteLine("返回值为Response为null，return");
-                return;
-            }
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.Default);
-            string retString = myStreamReader.ReadToEnd();
-            myStreamReader.Close();
-            myResponseStream.Close();
-            if (string.IsNullOrEmpty(retString))
-            {
-                Console.WriteLine("返回值为Response的html内容为空，return");
-                return;
-            }
-
-        }
-    }
-
-
-    public class Test
-    {
-        List<WebUrl> listStartUrl = new List<WebUrl>();
-        Queue<ListPage> queListPage = new Queue<ListPage>();
-        Queue<ContentPage> queContentPage = new Queue<ContentPage>();
-        Thread[] thread;
-        private log4net.ILog log;
-
-        public Test(log4net.ILog mlog)
-        {
-            log = mlog;
-        }
-
-        public void Start()
-        {
-            int i=0;
-            foreach (WebUrl wu in listStartUrl)
-            {
-                thread[i] = new Thread(new ParameterizedThreadStart(ProcessWebUrl));
-                thread[i].Name = "ProcessWebUrl" + i;
-                thread[i].IsBackground = true;
-                thread[i].Start(wu);
-                i++;
-            }
-
-        }
-
-        public void ProcessWebUrl(object weburl)
-        {
-            WebUrl wb = weburl as WebUrl;
-            if (null == wb)
-            {
-                log.InfoFormat("weburl对象为空", null);
-            }
-        }
-    }
-
-
-    public abstract class WebUrl
-    {
-        public string RegexName { get; set; }
-        public string Url { get; set; }
-        public Encoding PageEncoding { get; set; }
-    }
-
-    public class ListPage : WebUrl
-    {
-        public string ParamRegex { get; set; }
-        public string ListRegex { get; set; }
-        public string NextPageRegex { get; set; }
-    }
-
-    public class ContentPage : WebUrl
-    {
-        public List<Regex> ContentRegex { get; set; }
-    }
-
-    public class Regex
-    {
-        public string Name { get; set; }
-        public string RegexStr { get; set; }
-    }
+    #endregion
 }
